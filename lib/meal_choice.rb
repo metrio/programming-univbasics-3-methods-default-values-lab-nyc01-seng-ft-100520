a7987d4ee595b4810d5189b3137f5e8188bc1e72
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-15 19:18:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 
-def meal_choice (veg1, veg2, protien = 'meat')
+def meal_choice (veg1, veg2, protien = 'tofu')
   
   meal = "A plate of #{protien} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!"
